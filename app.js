377018b87463521c3febe9fe563dfdc6c7a4f424
pmaciej <pmaciej@users.noreply.github.com>
2020-04-03 19:09:39 +0200

    ff

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         diceDOM.src = 'dice-' + dice + '.png';
 
 
-// SZMATYYYYYyyfdsfsdfsddsfsdf
+// SZMATYYYYYyy
 
         if (dice !== 1) {
 
